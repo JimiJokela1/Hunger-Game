47a66795dddbecbde3e41f7755da278762fb2380
Georgios Riskas <giorgos.riskas@gmail.com>
2019-01-27 01:16:31 +0200

    - added music changes and ambient sound overlaps

--- Assets/Scripts/AudioArea.cs
+++ Assets/Scripts/AudioArea.cs
@@ -5,29 +5,41 @@ using UnityEngine;
 /// <summary>
 /// Plays the AudioSource attached to this gameObject when player enters the area
 /// </summary>
-[RequireComponent(typeof(AudioSource))]
 public class AudioArea : MonoBehaviour
 {
+    public enum AudioType {Music, Overlap};
 
-	//AudioSource environmentAudio;
- //   A
+    AudioSource thisAudioSource;
 
     public AudioClip audioClip;
 
-	//private void Awake()
-	//{
-	//	//environmentAudio = GetComponent<AudioSource>();
-	//}
+    public AudioType audioType;
 
-	private void OnTriggerEnter2D(Collider2D collision)
+    private void Awake()
+    {
+        if (audioType == AudioType.Overlap)
+        {
+            if (thisAudioSource == null)
+            {
+                thisAudioSource = gameObject.AddComponent<AudioSource>();
+            }
+            thisAudioSource.playOnAwake = false;
+            thisAudioSource.loop = true;
+            thisAudioSource.clip = audioClip;            
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
 	{
-        AudioManager.audioManager.ChangeTrack(audioClip);
-
         Debug.Log(this.GetType().FullName + ": Collision with " + collision);
         if (collision.tag != "Player")
             return;
 
-        //environmentAudio.Play();
+        if (audioType == AudioType.Music)
+            AudioManager.audioManager.ChangeTrack(audioClip);
+
+        if (audioType == AudioType.Overlap)
+            thisAudioSource.Play();
     }
 
 	void OnTriggerExit2D(Collider2D other)
@@ -37,7 +49,10 @@ public class AudioArea : MonoBehaviour
 		if (other.tag != "Player")
 			return;
 
-        AudioManager.audioManager.ChangeTrack(AudioManager.audioManager.mainTheme);
-		//environmentAudio.Stop();
+        if (audioType == AudioType.Overlap)
+            thisAudioSource.Stop();
+
+        if (audioType == AudioType.Music)
+            AudioManager.audioManager.ChangeTrack(AudioManager.audioManager.mainTheme);
 	}
 }
