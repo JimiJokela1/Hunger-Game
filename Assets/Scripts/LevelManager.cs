6a009b347ec8cdacd8be11e1d6765bd314e93295
Georgios Riskas <giorgos.riskas@gmail.com>
2019-01-26 20:45:18 +0200

    - added Start scene and level manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        //  Bad
+        if(SceneManager.GetActiveScene().name == "JimiScene")
+            SceneManager.LoadScene("UI", LoadSceneMode.Additive);
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+}
