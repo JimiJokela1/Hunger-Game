bf5bcdcc92c41169a82839882a538a14aab02b61
Unknown <jimi.jokela@metropolia.fi>
2019-01-26 18:30:55 +0200

    Add message when returning children home

--- Assets/Scripts/HomeArea.cs
+++ Assets/Scripts/HomeArea.cs
@@ -29,6 +29,22 @@ public class HomeArea : MonoBehaviour
             var hungerBar = FindObjectOfType<HungerBar>();
             hungerBar.currentValue += 20;
             hungerBar.SetValue(hungerBar.currentValue, hungerBar.maxValue);
+			// TODO:  and play sounds
+			// TODO: Give a small bonus to the player --> Fill the hunger bar by x amount
+
+			// Show text to player saying you brough home children
+			string message = "Grandma brought home and fed ";
+			// for (int i = 0; i < ChildManager.childManager.childrenAtHome.Count; i++)
+			// {
+			// 	message += ChildManager.childManager.childrenAtHome[i].ID;
+			// 	if (i + 1 < ChildManager.childManager.childrenAtHome.Count)
+			// 	{
+			// 		message += ", ";
+			// 	}
+			// }
+			SpeechArea.Instance.ShowText(message);
+		}
+	}
 
 
             // TODO:  and play sounds
@@ -53,6 +69,12 @@ public class HomeArea : MonoBehaviour
 
         // Change to different Audio environment...(?)
 
+		// TODO: Trigger countdown for children escaping(?)
+
+		// Hide message for bringing children home
+
+		SpeechArea.Instance.HideText();
+	}
 
         // TODO: Trigger countdown for children escaping(?)
 
