d76b70dbc4583207e64fb0b101b1d05b4e35a063
Georgios Riskas <giorgos.riskas@gmail.com>
2019-01-27 14:21:36 +0200

    - added grounging text

--- Assets/Scripts/Skills/GroundingSkill.cs
+++ Assets/Scripts/Skills/GroundingSkill.cs
@@ -15,6 +15,17 @@ public class GroundingSkill : Skill
         if (FindObjectOfType<GrandmaProperties>().grandmaIsAtHome)
         {
             SkillsHandler.skillsHandler.GroundCollectedChildren();
+            SpeechArea.Instance.ShowText("You’re Grounded! Now sit quietly and think about what you’ve done.");
         }
     }
+
+    IEnumerator ChildrenEscapedMessageTimer(string messageText)
+    {
+        yield return new WaitForSeconds(2);
+        if (SpeechArea.Instance.IsTextShowing(messageText))
+        {
+            SpeechArea.Instance.HideText();
+        }
+    }
+
 }
